cee1996bbaaf1cf7ea7794f3c1c6378446988c3a
PinliangLi <pinliang_li@outlook.com>
2020-09-18 16:34:37 +0200

    2_1

---
+++ 2_1/car_model.py
@@ -0,0 +1,37 @@
+import pygame
+
+
+class Car(pygame.sprite.Sprite):
+    """docstring for Car"""
+    
+    def __init__(self, speed = 0, engine_state = True):
+        super().__init__()
+        if speed > 11 :
+            speed = 11
+        elif speed < -11 :
+            speed = -11
+        self.speed = speed
+        self.engine_state = engine_state
+
+
+    def get_speed(self):
+        return self.speed
+    
+    def get_engine_state(self):
+        return self.engine_state
+
+    def set_speed(self, speed):
+        if speed > 11 :
+            speed = 11
+        elif speed < -11 :
+            speed = -11
+        self.speed = speed
+    
+
+    def reset_speed(self):
+        self.speed = 0
+
+
+    def change_engine_state(self):
+        self.engine_state = not self.engine_state
+
--- 2_1/keyikt_main.py
+++ 2_1/keyikt_main.py
@@ -4,10 +4,15 @@
 #                            Aufgabe 1                                #
 #######################################################################
 
-import pygame
+import sys, time
+import random
+import car_model
+import math
+import pygame       
 
-width = 400
-height = 200
+
+width = 800
+height = 600
 
 freq = 50  # Sets the frequency of input procession
 delta = 1.0 / freq # time per step
@@ -28,10 +33,11 @@ screen = pygame.display.set_mode((width, height))
 
 # get a clock to generate frequent behaviour
 clock = pygame.time.Clock()
-
+car = car_model.Car()
 
 # States of the keys
-keystates = {'quit':False}
+keystates = {'quit':False, 'up':False, 'down':False, 'reset_speed':False, 'engine_state':False, 'first_press': False}
+
 
 running = True
 try:
@@ -41,7 +47,8 @@ try:
         
         # save the last speed 4 analysis
         last = speed_cur
-     
+
+
         # process input events
         for event in pygame.event.get():
         
@@ -53,20 +60,84 @@ try:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q:
                     keystates['quit'] = True
+                if event.key == pygame.K_UP:
+                    if keystates['up'] == False:
+                        keystates['first_press'] = True
+                    if keystates['up'] == True:
+                        keystates['first_press'] = False
+
+                    keystates['up'] = True
+                if event.key == pygame.K_DOWN:
+                    if keystates['down'] == False:
+                        keystates['first_press'] = True
+                    if keystates['down'] == True:
+                        keystates['first_press'] = False
+                    keystates['down'] = True
+                if event.key == pygame.K_r:
+                    keystates['reset_speed'] = True
 
             # check for key up events (release)
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_q:
                     keystates['quit'] = False
+                if event.key == pygame.K_UP:
+                    keystates['up'] = False
+                    keystates['first_press'] = False
+                if event.key == pygame.K_DOWN:
+                    keystates['down'] = False
+                    keystates['first_press'] = False
+                if event.key == pygame.K_r:
+                    keystates['reset_speed'] = False
+                if event.key == pygame.K_s:
+                    car.change_engine_state()
+
 
         # do something about the key states here, now that the event queue has been processed
         if keystates['quit']:
             running = False
         
-        print("({},{} --> {})".format(speed_cur, angle_cur, (speed_cur - last) / delta))
+        if keystates['first_press'] == True:
+            acc=2.6
+            dec=4.5
+
+        if car.get_engine_state():
+            if keystates['up']:
+                speed = car.get_speed()
+                acc = acc - (acc/2) * (1 + math.erf( (math.fabs(speed) - 5.5) / (math.sqrt(2 * math.pow(2.5, 2))) ))
+                speed = speed + acc * delta
+                car.set_speed(speed)
+
+            if keystates['down']:
+                speed = car.get_speed()
+                dec = dec - (dec/2) * (1 + math.erf( (math.fabs(speed) - 5.5) / (math.sqrt(2 * math.pow(2.5, 2))) ))
+                speed = speed - dec * delta
+                car.set_speed(speed)
+        else :
+            if car.get_speed() > 0.05:
+                speed = car.get_speed()
+                frict = (frict/2) * (1 + math.erf( (math.fabs(speed) - 11/2) / (math.sqrt(2 * 4**2)) ))
+                speed = speed + frict * delta
+                car.set_speed(speed)
+            elif car.get_speed() < -0.05:
+                speed = car.get_speed()
+                frict = (frict/2) * (1 + math.erf( (math.fabs(speed) - 11/2) / (math.sqrt(2 * 4**2)) ))
+                speed = speed - frict * delta
+                car.set_speed(speed)
+            else :
+                speed = car.get_speed()
+                speed = 0
+                car.set_speed(speed)
+
+
+        if keystates['reset_speed']:
+            car.reset_speed()
+
+        print(keystates['first_press'])
+        speed_cur = car.get_speed()
+        print ("({},{} --> {})".format(speed_cur, angle_cur, (speed_cur - last) / delta))
     
 except KeyboardInterrupt:
-    print "Exiting through keyboard event (CTRL + C)"
+    print ("Exiting through keyboard event (CTRL + C)")
     
 # gracefully exit pygame here
 pygame.quit()
