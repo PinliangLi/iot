befed2dd4f5ccc3e71e8b41de54d104112b531b4
taotao <msyutao081@163.com>
2020-09-19 22:20:44 +0200

    change

--- 2_1/wiikt_main.py
+++ 2_1/wiikt_main.py
@@ -6,22 +6,25 @@
 
 from linuxWiimoteLib import *
 import bluetooth
-import servo_ctl
+import servo_ctrl
 import math
 import threading
 
-Real_Motor = servo_ctl.Motor()
-Real_Steering = servo_ctl.Real_Steering()
+Real_Motor = servo_ctrl.Motor()
+Real_Steering = servo_ctrl.Steering()
 One_speed = 5
-Two_speed = 10
+Two_speed = -5
 
 
 class Turning(threading.Thread):
 	"""docstring for Turning"""
-	def __init__(self, wii, omega = 0.5):
+	def __init__(self, wii, Steering, omega = 0.5, alpa = 0.5, n_pow = 2):
 		super(Turning, self).__init__()
-		self. wii = wii
+		self.wii = wii
 		self.omega = omega
+		self.n_pow = n_pow
+		self.alpa = alpa
+		self.Steering = Steering
 	
 	def run(self):
 
@@ -30,12 +33,13 @@ class Turning(threading.Thread):
 		while True:
 			rotate_x, rotate_y, rotate_z = wiimote.getGyroState()
 			new_rotate = self.omega * rotate_z + (1 - w) * last_rotate
-			actually_rotate = alpa * math.pow(new_rotate, n_pow) + (1 - alpa) * new_rotate
-			Real_Steering.set_angle(actually_rotate)
+			actually_rotate = self.alpa * math.pow(new_rotate, self.n_pow) + (1 - self.alpa) * new_rotate
+			self.Steering.set_angle(actually_rotate)
 			last_rotate = new_rotate
+			sleep(0.1)
 
 
-			
+print ("Prress SYNC on wiimote to make it discoverable")	
 
 
 
@@ -49,14 +53,13 @@ print("Found {} devices.".format(len(near_devices)))
 
 for addr, name in near_devices:
     print("{} --> {}".format(addr, name))
-    if "Joy" in name:
+    if "Nintendo" in name:
     	device = (addr, name)
     	print(device)
 
 connected = False
 
 try:
-	print ("Press SYNC on wiimote to make it discoverable")
 	while (not connected):
 		connected = wiimote.Connect(device)
 
@@ -66,7 +69,7 @@ try:
 
 	wiistate = wiimote.WiimoteState
 
-	turning = Turning(wiimote)
+	turning = Turning(wiimote, Real_Steering)
 	while True:
 
 		# re-calibrate accelerometer
@@ -96,16 +99,16 @@ try:
 			print("angle:", Real_Steering.get_angle())
 
 		if (wiistate.ButtonState.One):
-			print("One Button Down Speed:" One_speed)
+			print("One Button Down Speed:", One_speed)
 			Real_Motor.set_speed(One_speed)
 
 		if (wiistate.ButtonState.Two):
-			print("Two Button Down Speed:" Two_speed)
+			print("Two Button Down Speed:", Two_speed)
 			Real_Motor.set_speed(Two_speed)
 		
 		Speed = Real_Motor.get_speed()
 
-		if 0 <= math.fabs(Speed) <= 4:
+		if 0 < math.fabs(Speed) <= 4:
 			wiimote.SetLEDs(True,False,False,False)
 
 		elif 4 < math.fabs(Speed) <= 8:
@@ -114,25 +117,28 @@ try:
 		elif 8 < math.fabs(Speed) < 11:
 			wiimote.SetLEDs(True,True,True,False)			
 
-		elif Speed == 11
+		elif math.fabs(Speed) == 11:
 			wiimote.SetLEDs(True,True,True,True)
 
 		else :
-			wiimote.SetLEDs(True,False,False,False)
+			wiimote.SetLEDs(False,False,False,False)
 
 		if (wiistate.ButtonState.A):
 			print("Reset")
 			Real_Motor.set_speed(0)
 			Real_Steering.set_angle(0)
 
+		
 		if (not wiistate.ButtonState.Home):
-			triple(acc_X, acc_Y, acc_Z) = wiimote.getAccelState()
+			acc_X, acc_Y, acc_Z = wiimote.getAccelState()
 			print ("({},{},{})".format(acc_X, acc_Y, acc_Z))
 		else:
-			print 're-calibrating'
+			print ('re-calibrating')
 			wiimote.calibrateAccelerometer()
+
+
 		if (wiistate.ButtonState.B):
-			triple(angle_X, angle_Y, angle_Z) = wiimote.getGyroState()
+			angle_X, angle_Y, angle_Z = wiimote.getGyroState()
 			if angle_Y == 0:
 				speed_cur = 0
 			elif -45 < angle_Y < 0:
